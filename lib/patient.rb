64b8b276b41d68918999f76d98f22e54864cbaad
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 18:32:22 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient 
+
+  attr_accessor :name 
+
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+  end
+
+  def new_appointment(doctor, date)
+    Appointent.new(doctor, self, date)
+  end
+
+  
+end
