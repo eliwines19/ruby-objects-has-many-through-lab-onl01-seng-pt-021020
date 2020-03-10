941f0e24fad1f5433f7108831d70a471f9d00e79
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 18:40:18 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,17 +7,21 @@ class Genre
     @name = name
     @@all << self
   end
+  #has a name 
 
   def self.all
     @@all
   end
+  #knowns about all genre instances 
 
   def songs
     Song.all.select { |s| s.genre == self}
   end
+  #has many songs 
 
   def artists
     Song.all.select { |s| s.genre == self}.map {|m| m.artist}
   end
+  #has many artists, throughb songs 
 
 end
